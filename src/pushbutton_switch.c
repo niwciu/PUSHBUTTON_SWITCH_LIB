576018e84dc4b41fdc8592d9057bbe927f55fdb3
niwciu <57457942+niwciu@users.noreply.github.com>
2024-02-27 14:31:08 +0100

    Src: REAFCTOR

--- src/pushbutton_switch.c
+++ src/pushbutton_switch.c
@@ -20,17 +20,19 @@ typedef uint16_t debounce_repetition_timer_t;
 
 typedef struct
 {
-    pushbutton_driver_interface_t *GPIO_interface;                                         
+    pushbutton_driver_interface_t *GPIO_interface;
     debounce_repetition_timer_t deb_rep_timer;
     uint8_t REPETITION_FLAG;
     pushbutton_callback_t push_callback;
     pushbutton_callback_t up_callback;
     //
 } pushbutton_t;
-
 pushbutton_t PUSHBUTTON_1;
 pushbutton_t PUSHBUTTON_2;
+
 static pushbutton_t *get_pushbutton_struct_adres(enum pushbutton_e button_name);
+static void update_button_repetition_counter(pushbutton_t *BUTTON, enum pushbutton_repetition_e repetition);
+static void debounce_pushbutton_state(pushbutton_t *BUTTON, enum pushbutton_repetition_e repetition);
 
 static pushbutton_t *get_pushbutton_struct_adres(enum pushbutton_e button_name)
 {
@@ -49,6 +51,48 @@ static pushbutton_t *get_pushbutton_struct_adres(enum pushbutton_e button_name)
     return BUTTON;
 }
 
+static void update_button_repetition_counter(pushbutton_t *BUTTON, enum pushbutton_repetition_e repetition)
+{
+
+    if (repetition == REPETITION_ON)
+    {
+        if ((BUTTON->REPETITION_FLAG) == 0)
+        {
+            BUTTON->deb_rep_timer = PUSHBUTTON_FIRST_REPETITION_TIME;
+            BUTTON->REPETITION_FLAG = 1;
+        }
+        else
+        {
+            BUTTON->deb_rep_timer = PUSHBUTTON_CONTINUES_REPETITION_TIME;
+        }
+    }
+    else
+    {
+        BUTTON->deb_rep_timer = 0;
+    }
+}
+
+static void debounce_pushbutton_state(pushbutton_t *BUTTON, enum pushbutton_repetition_e repetition)
+{
+    enum button_state_e BUTTON_input_state = BUTTON->GPIO_interface->get_button_state();
+    if (BUTTON_input_state == PUSHED)
+    {
+        if ((BUTTON->deb_rep_timer) == 1)
+        {
+            if (BUTTON->push_callback != NULL)
+                BUTTON->push_callback();
+            update_button_repetition_counter(BUTTON, repetition);
+        }
+        else
+        {
+        }
+    }
+    else
+    {
+        BUTTON->deb_rep_timer = PUSHBUTTON_DEBOUNCE_TIME;
+        BUTTON->REPETITION_FLAG = 0;
+    }
+}
 /**
  * @brief  Function that initialize hardwer se they can work as inputs
  */
@@ -56,54 +100,17 @@ void init_pushbuttons(void)
 {
     PUSHBUTTON_1.GPIO_interface = pushbutton_1_GPIO_interface_get();
     PUSHBUTTON_2.GPIO_interface = pushbutton_2_GPIO_interface_get();
-
     PUSHBUTTON_1.GPIO_interface->GPIO_init();
     PUSHBUTTON_2.GPIO_interface->GPIO_init();
-
-    // PUSHBUTTON_1.debounce_timer=PUSHBUTTON_DEBOUNCE_TIME;
-    // PUSHBUTTON_2.debounce_timer=PUSHBUTTON_DEBOUNCE_TIME;
 }
 
-void check_button_push(enum pushbutton_e button_name,enum pushbutton_repetition_e repetition)
+void check_button_push(enum pushbutton_e button_name, enum pushbutton_repetition_e repetition)
 {
     pushbutton_t *BUTTON = get_pushbutton_struct_adres(button_name);
 
     if (BUTTON != NULL)
     {
-        enum button_state_e BUTTON_input_state = BUTTON->GPIO_interface->get_button_state();
-        if (BUTTON_input_state == PUSHED)
-        {
-            if ((BUTTON->deb_rep_timer) == 1)
-            {
-                if (BUTTON->push_callback != NULL)
-                    BUTTON->push_callback();
-                
-                if(repetition==REPETITION_ON)
-                {
-                    if((BUTTON->REPETITION_FLAG)==0)
-                    {
-                        BUTTON->deb_rep_timer = PUSHBUTTON_FIRST_REPETITION_TIME;
-                        BUTTON->REPETITION_FLAG=1;
-                    }
-                    else
-                    {
-                         BUTTON->deb_rep_timer = PUSHBUTTON_CONTINUES_REPETITION_TIME;
-                    }
-                }
-                else
-                {
-                    BUTTON->deb_rep_timer = 0;
-                }
-            }
-            else
-            {
-            }
-        }
-        else
-        {
-            BUTTON->deb_rep_timer = PUSHBUTTON_DEBOUNCE_TIME;
-            BUTTON->REPETITION_FLAG=0;
-        }
+        debounce_pushbutton_state(BUTTON, repetition);
     }
 }
 
