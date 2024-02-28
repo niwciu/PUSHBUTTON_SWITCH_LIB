2b6d42a4c997efcaf186609dc3fbd842edc3c418
niwciu <57457942+niwciu@users.noreply.github.com>
2024-02-28 18:31:10 +0100

    Test: GREEN added test ->
    GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndReleaseAndPinBounceInTimeShorterThanDebounceTimeThenTestTimerEqual0

--- test/pushbutton/pushbutton_test.c
+++ test/pushbutton/pushbutton_test.c
@@ -447,7 +447,7 @@ TEST(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndUpPinS
     TEST_ASSERT_EQUAL(1,TEST_TIMER);
 }
 
-TEST(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndUpPinStateIsStableForLestThanDebounceTimeThenChangeTestTimerEqual0)
+TEST(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndUpPinStateIsStableForLessThanDebounceTimeThenTestTimerEqual0)
 {
     register_button_push_or_release_callback(BUTTON_1,inc_test_timer);
     check_button_release(BUTTON_1);
@@ -462,11 +462,48 @@ TEST(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndUpPinS
     TEST_ASSERT_EQUAL(0,TEST_TIMER);
 }
 
-// TEST(pushbutton, )
-// {
+TEST(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndReleaseAndPinBounceInTimeShorterThanDebounceTimeThenTestTimerEqual0)
+{
+    register_button_push_or_release_callback(BUTTON_1,inc_test_timer);
+    check_button_release(BUTTON_1);
+    PUSHBUTTON_1_STATE=PUSHED;
+    check_button_release(BUTTON_1);
 
-//     TEST_FAIL_MESSAGE("added new test")
-// }
+    PUSHBUTTON_1_STATE=RELEASED;
+    check_button_release(BUTTON_1);
+    generate_pushbutton_deb_rep_timer_delay(PUSHBUTTON_DEBOUNCE_TIME-2,BUTTON_1);
+    check_button_release(BUTTON_1);
+
+    PUSHBUTTON_1_STATE=PUSHED;
+    check_button_release(BUTTON_1);
+    generate_pushbutton_deb_rep_timer_delay(PUSHBUTTON_DEBOUNCE_TIME-2,BUTTON_1);
+    check_button_release(BUTTON_1);
+
+    PUSHBUTTON_1_STATE=RELEASED;
+    check_button_release(BUTTON_1);
+    generate_pushbutton_deb_rep_timer_delay(10,BUTTON_1);
+    check_button_release(BUTTON_1);
+    PUSHBUTTON_1_STATE=PUSHED;
+    check_button_release(BUTTON_1);
+    generate_pushbutton_deb_rep_timer_delay(1,BUTTON_1);
+    check_button_release(BUTTON_1);
+
+    PUSHBUTTON_1_STATE=RELEASED;
+    check_button_release(BUTTON_1);
+    generate_pushbutton_deb_rep_timer_delay(6,BUTTON_1);
+    check_button_release(BUTTON_1);
+    PUSHBUTTON_1_STATE=PUSHED;
+    check_button_release(BUTTON_1);
+    generate_pushbutton_deb_rep_timer_delay(84,BUTTON_1);
+    check_button_release(BUTTON_1);
+
+    PUSHBUTTON_1_STATE=RELEASED;
+    check_button_release(BUTTON_1);
+    generate_pushbutton_deb_rep_timer_delay(PUSHBUTTON_DEBOUNCE_TIME-2,BUTTON_1);
+    check_button_release(BUTTON_1);
+
+    TEST_ASSERT_EQUAL(0,TEST_TIMER);
+}
 
 // TEST(pushbutton, )
 // {
--- test/pushbutton/pushbutton_test_runner.c
+++ test/pushbutton/pushbutton_test_runner.c
@@ -17,8 +17,8 @@ TEST_GROUP_RUNNER(pushbutton)
    RUN_TEST_CASE(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushForTimeLessToReachDoubleRepetitionThenTestTimerValueIs2);
    RUN_TEST_CASE(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushForTimeLongerToReachDoubleRepetitionThenTestTimerValueIs3);
    RUN_TEST_CASE(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndUpPinStateIsStableForDebounceTimeThenChangeIncTestTimer);
-   RUN_TEST_CASE(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndUpPinStateIsStableForLestThanDebounceTimeThenChangeTestTimerEqual0);
-   // RUN_TEST_CASE(pushbutton, );
+   RUN_TEST_CASE(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndUpPinStateIsStableForLessThanDebounceTimeThenTestTimerEqual0);
+   RUN_TEST_CASE(pushbutton, GivenKey1InitAndIncTimerFunctionRegisteredWhenKey1PushAndReleaseAndPinBounceInTimeShorterThanDebounceTimeThenTestTimerEqual0);
    // RUN_TEST_CASE(pushbutton, );
    // RUN_TEST_CASE(pushbutton, );
    // RUN_TEST_CASE(pushbutton, );
