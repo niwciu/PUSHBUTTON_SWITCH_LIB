456a73e39505e4ba7092597c66f5abb5638dd1a3
niwciu <57457942+niwciu@users.noreply.github.com>
2024-03-04 19:02:14 +0100

    Test: GREEN -> added new test ->
    GivenSwitchIsOffAndTestTimerEqual10AndSW1InitAndIncTimerFunctionRegisteredToSwOnAndSW1SwitchedOnAndBouncingWhenSW1StableForDebounceTimePlus1ThenTestTimerEqual11

--- test/switch/switch_test.c
+++ test/switch/switch_test.c
@@ -75,10 +75,8 @@ TEST(Switch, GivenSwitchIsOffAndTestTimerEqual10AndSW1InitAndIncTimerFunctionReg
     mock_SWITCH_1_STATE=SWITCH_INPUT_OFF;
     init_switch(&SW_1,switch_1_GPIO_interface_get);
     check_switch(&SW_1);
-    check_switch(&SW_1);
     register_switch_ON_callback(&SW_1,inc_test_timer);
     mock_SWITCH_1_STATE=SWITCH_INPUT_ON;
-    check_switch(&SW_1);
     check_switch_X_times(&SW_1,SWITCH_DEBOUNCE_REPETITIONS+1);
     TEST_ASSERT_EQUAL(11,TEST_TIMER);
 }
