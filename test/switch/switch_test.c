a4bd662caa581d2528f156bf16460842b4baa568
niwciu <57457942+niwciu@users.noreply.github.com>
2024-03-04 17:29:12 +0100

    Test: RED -> added test GivenSW1InitAndIncTimerFunctionRegisteredToSwOnAndSW1SwitchedOnAndBouncingWhenSW1StableForDebounceTimeThenTestTimerEqual1

--- test/switch/switch_test.c
+++ test/switch/switch_test.c
@@ -1,16 +1,20 @@
 #include "unity/fixture/unity_fixture.h"
 #include "mock_switch_GPIO_driver.h"
 #include "switch.h"
-// #include "switch_GPIO_interface.h"
 
-// #include "tested_module.h"
+
+typedef uint16_t debounce_repetition_t;
 
 SWITCH_TypDef SW_1;
+
+
+static void check_switch_X_times(SWITCH_TypDef *SWITCH, debounce_repetition_t repetition);
 
 TEST_GROUP(Switch);
 
 TEST_SETUP(Switch)
 {
+    init_switch(&SW_1,switch_1_GPIO_interface_get);
     /* Init before every test */
 }
 
@@ -21,10 +25,19 @@ TEST_TEAR_DOWN(Switch)
 
 TEST(Switch, WhenSwitchInitThenInputSwitchStateEqualToSwitchOFF)
 {
-    init_switch(&SW_1,switch_1_GPIO_interface_get);
-    TEST_ASSERT_EQUAL(SWITCH_OFF,mock_SWITCH_1_STATE);
+    
+    TEST_ASSERT_EQUAL(SWITCH_INPUT_OFF,mock_SWITCH_1_STATE);
 }
-
+TEST(Switch, GivenSW1InitAndIncTimerFunctionRegisteredToSwOnAndSW1SwitchedOnAndBouncingWhenSW1StableForDebounceTimeThenTestTimerEqual1)
+{
+    //Given
+    register_switch_ON_callback(&SW_1,inc_test_timer);
+    mock_SWITCH_1_STATE=SWITCH_INPUT_ON;
+    check_switch(&SW_1);
+    check_switch_X_times(&SW_1,SWITCH_DEBOUNCE_REPETITIONS);
+    check_switch(&SW_1);
+    TEST_ASSERT_EQUAL(1,TEST_TIMER);
+}
 // TEST(Switch, )
 // {
 //     TEST_FAIL_MESSAGE("Implement your test!");
@@ -69,7 +82,11 @@ TEST(Switch, WhenSwitchInitThenInputSwitchStateEqualToSwitchOFF)
 // {
 //     TEST_FAIL_MESSAGE("Implement your test!");
 // }
-// TEST(Switch, )
-// {
-//     TEST_FAIL_MESSAGE("Implement your test!");
-// }
+static void check_switch_X_times(SWITCH_TypDef *SWITCH, debounce_repetition_t repetition)
+{
+    while(repetition)
+    {
+        repetition--;
+        check_switch(SWITCH);
+    }
+}
--- test/switch/switch_test_runner.c
+++ test/switch/switch_test_runner.c
@@ -4,4 +4,23 @@ TEST_GROUP_RUNNER(Switch)
 {
    /* Test cases to run */
    RUN_TEST_CASE(Switch, WhenSwitchInitThenInputSwitchStateEqualToSwitchOFF);
+   RUN_TEST_CASE(Switch, GivenSW1InitAndIncTimerFunctionRegisteredToSwOnAndSW1SwitchedOnAndBouncingWhenSW1StableForDebounceTimeThenTestTimerEqual1);
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+   // RUN_TEST_CASE(Switch, );
+
 }
