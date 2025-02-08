2ecbb41df37b7b714a02663376fa7cf2826f2ba1
niwciu <niwciu@gmail.com>
2025-02-08 21:41:06 +0100

    PROJECT: structure and configuration changed
    added new functionality in custo targets
    added new CI pipeline
    rename examples folders to correct ones
    added dev container

--- test/switch/custom_targets.cmake
+++ test/switch/custom_targets.cmake
@@ -15,43 +15,128 @@ add_custom_target(run switch_test)
 # check if lizard software is available 
 find_program(lizard_program lizard)
 if(lizard_program)
-	message(STATUS "Lizard was found, you can use predefined targets for SWITCH src folder Code Complexity Metrics: \r\n\tccm,\r\n\tccmr,")
+	message(STATUS "Lizard was found, you can use predefined targets for src folder Code Complexity Metrics: \r\n\tccm,\r\n\tccmr,")
 else()
-	message(STATUS "Lizard was not found. \r\n\tInstall Lizard to get predefined targets for SWITCH src folder Code Complexity Metrics")
+	message(STATUS "Lizard was not found. \r\n\tInstall Lizard to get predefined targets for src folder Code Complexity Metrics")
 endif()
 # Prints CCM for src folder in the console
-add_custom_target(ccm lizard ../../../src/SWITCH/ --CCN 12 -Tnloc=30 -a 4 --languages cpp -V -i 1)
+add_custom_target(ccm lizard 
+						../../../src/switch 
+						--CCN 12 -Tnloc=30 
+						-a 4 
+						--languages cpp 
+						-V 
+						-i 1)
 # Create CCM report in reports/Cylcomatic_Complexity/
-add_custom_target(ccmr lizard ../../../src/SWITCH --CCN 12 -Tnloc=30 -a 4 --languages cpp -V -o ../../../reports/Cyclomatic_Complexity/SWITCH/Lizard_report.html)
+add_custom_command(
+    OUTPUT ../../../reports/CCM/
+    COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCM/
+    COMMENT "Tworzenie katalogów raportów Code Coverage"
+)
+add_custom_target(ccmr 
+	COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCM/
+	COMMAND lizard 
+				../../../src/switch 
+				--CCN 12 
+				-Tnloc=30 
+				-a 4 
+				--languages cpp 
+				-V 
+				-o ../../../reports/CCM/switch.html
+)
 
 # TARGET FOR MAKING STATIC ANALYSIS OF THE SOURCE CODE AND UNIT TEST CODE
 # check if cppchec software is available 
 find_program(cppcheck_program cppcheck)
 if(cppcheck_program)
-	message(STATUS "CppCheck was found, you can use predefined targets for static analize : \r\n\tcppcheck_src,\r\n\tcppcheck_test,")
+	message(STATUS "CppCheck was found, you can use predefined targets for static analize : \r\n\tcppcheck,")
 else()
 	message(STATUS "CppCheck was not found. \r\n\tInstall CppCheck to get predefined targets for static analize")
 endif()
-# Prints static analize output for src folder in the console
-add_custom_target(cppcheck_src cppcheck ../../../src/SWITCH/ --enable=all --inconclusive --force --inline-suppr --platform=win64 --suppress=missingInclude --suppress=missingIncludeSystem --suppress=unusedFunction)
-# Prints static analize output for specific test_module folder in the console
-add_custom_target(cppcheck_test cppcheck ../../../test/switch -itest/switch/out -itest/switch/out_avr --enable=all --inconclusive --force --inline-suppr --platform=win64 --suppress=missingInclude --suppress=missingIncludeSystem || echo "test")
-
+add_custom_target(cppcheck cppcheck
+					../../../src/switch
+					../../../test/switch
+					-i../../../test/switch/out
+					--enable=all
+					--force
+					# --inconclusive
+					--std=c99
+					# --inline-suppr 
+					# --platform=win64 
+					--suppress=missingIncludeSystem 
+					--suppress=missingInclude
+					# --suppress=unusedFunction:../../../test/switch/switch_test_runner.c:3
+					# --checkers-report=cppcheck_checkers_report.txt
+					)
 # TARGET FOR CREATING CODE COVERAGE REPORTS
 # check if python 3 and gcovr are available 
 find_program(GCOVR gcovr)
-find_program(PYTHON3 python3)
-if(PYTHON3)
-	if(GCOVR)
-		message(STATUS "python 3 and gcovr was found, you can use predefined targets for uint tests code coverage report generation : \r\n\tccr,")
-	else()
-		message(STATUS "pyton 3 was found but gcovr was not found. \r\n\tInstall gcovr to get predefined targets for uint tests code coverage report generation")
-	endif()
+if(GCOVR)
+	message(STATUS "python 3 and gcovr was found, you can use predefined targets for uint tests code coverage report generation : 
+					\r\tccc - Code Coverage Check, 
+					\r\tccr - Code Coverage Reports generation,
+					\r\tccca - Code Coverage Check All -> whole project check, 
+					\r\tccra - Code Coverage Reports All -> whole project raport generation")
 else()
-	if(GCOVR)
-		message(STATUS "python3 was not found. \r\n\tInstall python 3 to get predefined targets for uint tests code coverage report generation")
-	else()
-		message(STATUS "python3 and gcovr were not found. \r\n\tInstall python 3 and gcovr to get predefined targets for uint tests code coverage report generation")
-	endif()
+	message(STATUS "pyton 3 was found but gcovr was not found. \r\n\tInstall gcovr to get predefined targets for uint tests code coverage report generation")
 endif()
-add_custom_target(ccr python3 -m gcovr CMakeFiles/switch_test.dir/D_/EMBEDDED/LIBRARIES/C_libraries/PUSHBUTTON_SWITCH_LIB/src/SWITCH -r ../../.. --html-details ../../../reports/Code_Coverage/SWITCH/switch_gcov_report.html)
+add_custom_command(
+    OUTPUT ../../../reports/CCR/switch/ ../../../reports/CCR/JSON_ALL/ ../../../reports/CCR/HTML_OUT/
+    COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCR/switch/
+    COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCR/JSON_ALL/
+	COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCR/HTML_OUT/
+    COMMENT "Tworzenie katalogów raportów Code Coverage"
+)
+add_custom_target(ccr
+	COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCR/switch/
+	COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCR/JSON_ALL/
+	COMMAND gcovr 
+				-r ../../../src/switch 
+				--json ../../../reports/CCR/JSON_ALL/coverage_switch.json
+				--json-base  src/switch
+				--html-details ../../../reports/CCR/switch/switch_report.html 
+				.
+)
+		
+add_custom_target(ccc gcovr  
+						-r ../../../src/switch 
+						--fail-under-line 90
+						.
+)
+
+add_custom_target(ccca gcovr  
+						-r ../../../ 
+						--json-add-tracefile \"../../../reports/CCR/JSON_ALL/coverage_*.json\"  
+						.
+)
+						
+add_custom_target(ccra  
+	COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCR/HTML_OUT/
+	COMMAND ${CMAKE_COMMAND} -E make_directory ../../../reports/CCR/JSON_ALL/
+	COMMAND gcovr 
+				-r ../../../ 
+				--json-add-tracefile \"../../../reports/CCR/JSON_ALL/coverage_*.json\"  
+				--html-details -o ../../../reports/CCR/HTML_OUT/project_coverage.html
+				.
+)
+add_dependencies(ccra ccr)
+add_dependencies(ccca ccr)
+
+find_program(CLANG_FORMAT clang-format)
+if(CLANG_FORMAT)
+	message(STATUS "clang-format was found, you can use predefined target for formating the code in project predefined standard : \r\n\tformat \r\n\tformat_test")
+else()
+	message(STATUS "clang-format was not found. \r\n\tInstall clang-format to get predefined target for formating the code in project predefined standard")
+endif()
+add_custom_target(format  clang-format 
+							-i 
+							-style=file 
+							../../../src/switch/*.c 
+							../../../src/switch/*.h
+)
+add_custom_target(format_test  clang-format 
+								-i 
+								-style=file 
+								../*.c 
+								../*.h
+)
